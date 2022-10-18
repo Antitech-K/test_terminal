1a2f63231206400dbb3b75a16a4f1e2c7879a92d
Antitech-K <kononov_mt2@mail.ru>
2022-10-19 01:31:50 +0300

    button

--- locators/forms_pages_locators.py
+++ locators/forms_pages_locators.py
@@ -1,10 +1,23 @@
 import pyautogui
 
 class FormsPagesLocators:
-    START_AUTHORIZATION = pyautogui.leftClick(1030, 185)
-    PRINT_STIKCERS = pyautogui.leftClick(1030, 300)
-    ACQUAINTANCE_PAC = pyautogui.leftClick(1030, 430)
-    HELPING = pyautogui.leftClick(1225, 45)
-    NUMBER_0 = pyautogui.leftClick(1055, 585)
-    NUMBER_1 = pyautogui.leftClick(890, 305)
-    NUMBER_2 = pyautogui.leftClick(1050, 305)
+
+    def START_AUTHORIZATION():
+        START_AUTHORIZATION = pyautogui.leftClick(1030, 185, duration=1)
+        pass
+    def ACQUAINTANCE_PAC():
+        ACQUAINTANCE_PAC = pyautogui.leftClick(1030, 430, duration=1)
+        pass
+    def HELPING():
+        HELPING = pyautogui.leftClick(1225, 45)
+        pass
+    def NUMBER_0():
+        NUMBER_0 = pyautogui.leftClick(1055, 585)
+        pass
+    def NUMBER_1():
+        NUMBER_1 = pyautogui.leftClick(890, 305)
+        pass
+    def NUMBER_2():
+        NUMBER_2 = pyautogui.leftClick(1050, 305)
+        pass
+    
