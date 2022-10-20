35d00ffebea9af5a1dc18f9ca32972c83170b3dc
Antitech-K <kononov_mt2@mail.ru>
2022-10-20 17:03:14 +0300

    counter on starting

--- pages/start_page.py
+++ pages/start_page.py
@@ -6,7 +6,12 @@ import os
 class Load_start:
     os.system("DISPLAY=:0 NODE_ENV=production aurora")
     time.sleep(2)
+    counter = 0
     while pyautogui.locateCenterOnScreen('start_load.png', confidence=0.99) ==None:
         time.sleep(1)
+        counter += 1
+        if counter == 10:
+            print("Хюстон, у нас проблемы на экране термометрия")
+
     pyautogui.press('f11') #Нажимаем клавишу F11
     time.sleep(0.5)
