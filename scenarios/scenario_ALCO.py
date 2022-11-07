bee4518c61a3ae3c646988276485fc4d05b3cccd
Antitech-K <kononov_mt2@mail.ru>
2022-11-07 18:13:02 +0300

    3

--- scenarios/scenario_ALCO.py
+++ scenarios/scenario_ALCO.py
@@ -4,6 +4,7 @@ import pyautogui
 
 class Alco:
     def runDingoE200():
+        time.sleep(10)
         os.system("/opt/RODOS4/RODOS4 --id 4798 --c0 128")
         time.sleep(2)
         os.system("/opt/RODOS4/RODOS4 --id 4798 --c0 0")
--- scenarios/scenariо_berrcom.py
+++ scenarios/scenariо_berrcom.py
@@ -11,6 +11,7 @@ class Termometr:
             temp += 1
             if temp == 20:
                 return print("Хюстон, у нас проблемы на экране термометрии")
+        time.sleep(1)
         os.system("/opt/RODOS4/RODOS4 --id 4798 --c9 128")
         time.sleep(0.5)
         os.system("/opt/RODOS4/RODOS4 --id 4798 --c9 0")
